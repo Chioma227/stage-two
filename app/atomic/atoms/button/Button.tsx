8c82db3a6abc62a98509d8686fc31e12771ef28f
Mary <101870828+Chioma227@users.noreply.github.com>
2024-07-09 08:29:40 +0100

    Update Button.tsx

--- app/atomic/atoms/button/Button.tsx
+++ app/atomic/atoms/button/Button.tsx
@@ -13,13 +13,13 @@ const Button = ({ children, onClick, className, variant }: bProps) => {
     switch (variant) {
         case buttonVariants.DEFAULT:
             style = clsx(className,
-                "bg-orange  whitespace-nowrap w-fit",
+                "bg-orange hover:bg[#D36506] whitespace-nowrap w-fit",
                 "rounded-[4px] text-[14px]  py-[9px] sm:px-[2.3rem] px-[2rem]"
             )
             break;
         case buttonVariants.DEFAULT_FULL:
             style = clsx(className,
-                "bg-orange whitespace-nowrap w-[100%]",
+                "bg-orange hover:bg[#D36506] whitespace-nowrap w-[100%]",
                 "rounded-[4px] text-[14px] py-[9px] px-[2.3rem]"
             )
             break;
